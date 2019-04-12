3cd9ee408b0c0503d2b0ae404e01ff5082864bbc
Artem Miroshnichenko <a.miroshnichenko@epom.com>
2019-04-12 18:29:45 +0300

    Get vars list via expand test

--- result.go
+++ result.go
@@ -33,7 +33,7 @@ type Result struct {
 	State                  string `json:"state"`
 	BuildState             string `json:"buildState"`
 	Number                 int    `json:"number"`
-	Variables              string `json:"variables"`
+	Variables              string `json:"variableContext"`
 	BuildNumber            int    `json:"buildNumber"`
 }
 
